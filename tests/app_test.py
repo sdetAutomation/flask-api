f26efff4e52cdc6768795676fffa31a503446f74
sdetAutomation <sdet.testautomation@gmail.com>
2018-10-27 14:03:53 -0500

    add test to ci

---
+++ tests/app_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class AppTest(unittest.TestCase):
+    def test_tc0001_smoketest(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
